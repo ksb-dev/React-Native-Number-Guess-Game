b3aa82b41de780c87120b215808cef1c1679e070
Kedar Babaleshwar <babaleshwarkedar@gmail.com>
2022-08-26 11:23:02 +0530

    Initial Commit

---
+++ App.js
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+
+// components
+import StartGameScreen from './screens/StartGameScreen'
+
+export default function App () {
+  return <StartGameScreen />
+}
+
+const styles = StyleSheet.create({})
